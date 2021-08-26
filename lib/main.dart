649e3fe34653935b149f937f135a7a21511873a9
truongan99 <81751920+truongan99@users.noreply.github.com>
2021-08-26 14:32:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(fontFamily: 'Helvetica'),
-      home: SleepMusic(),
+      home: CourseDetail(),
     );
   }
 }
